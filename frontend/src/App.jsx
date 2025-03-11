dfc6a7dcdfe4e9e5eeb041503fdaf64a29b8463f
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-03-11 11:40:09 +0200

    add user locating

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,50 @@
+import { useRef, useState } from 'react'
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
-import { useMap } from 'react-leaflet/hooks'
+import { useMap, useMapEvents } from 'react-leaflet/hooks'
 import { Marker } from 'react-leaflet/Marker'
 import { Popup } from 'react-leaflet/Popup'
-import { useRef } from 'react'
+
+
+
+const LocationMarker = ({ position, setPosition }) => {
+
+  const map = useMapEvents({
+    locationfound: (e) => {
+      setPosition(e.latlng)
+    },
+  }) 
+  
+  setInterval(() => {
+    map.locate()
+  }, 5000)
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>Your current location</Popup>
+    </Marker>
+  )
+}
 
 const App = () => {
+  const [position, setPosition] = useState(null)
+
   const mapRef = useRef(null);
   const latitude = 51.505;
   const longitude = -0.09;
-
+  
   return (
-    <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{height: "100vh", width: "100vw"}}>
+    <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{height: "98.2vh", width: "99.2vw"}}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker position={[latitude, longitude]}>
+        <Popup>
+          L I dkkd
+        </Popup>
+      </Marker>
+      <LocationMarker map={mapRef} position={position} setPosition={setPosition} />
     </MapContainer>
   )
 }
