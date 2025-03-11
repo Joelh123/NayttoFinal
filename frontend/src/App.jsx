bda4c4e5560057448e634832c7516844607350f6
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-03-11 08:42:22 +0200

    Add leaflet map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+import { useMap } from 'react-leaflet/hooks'
+import { Marker } from 'react-leaflet/Marker'
+import { Popup } from 'react-leaflet/Popup'
+import { useRef } from 'react'
+
 const App = () => {
-  
+  const mapRef = useRef(null);
+  const latitude = 51.505;
+  const longitude = -0.09;
 
   return (
-    <div>
-      <p>Hello world</p>
-    </div>
+    <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{height: "100vh", width: "100vw"}}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
   )
 }
 
